7141ac04014a3713e2d041601d3c89efc8066c4f
Paloma <paloma.aubeau@gmail.com>
2024-06-19 11:24:46 +0200

    PATH modifié avec back déployé

--- frontend/utils/path.js
+++ frontend/utils/path.js
@@ -1,3 +1,3 @@
 // export const PATH = "http://localhost:3000"; // Paloma sur émulateur
-export const PATH = "http://192.168.1.26:3000"; // Paloma sur téléphone
-//export const PATH = "https://easplit-backend.vercel.app"; // Back déployé
+//export const PATH = "http://192.168.1.26:3000"; // Paloma sur téléphone
+export const PATH = "https://easplit-paloma-aubeaus-projects.vercel.app"; // Back déployé
